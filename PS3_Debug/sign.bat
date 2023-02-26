09885ef7741dfe29f92fbc655c0275a3483c1b0a
Nathan Martin <nathan_r32_69@hotmail.com>
2023-02-26 23:25:22 +0100

    Deleted PS3_Debug folder

--- PS3_Debug/sign.bat
+++
@@ -1,7 +0,0 @@
-del xai_plugin.sprx
-del videorec.sprx
-
-scetool -0 SELF -1 TRUE -s FALSE -2 04 -3 1070000052000001 -4 01000002 -5 APP -6 0003004000000000 -A 0001000000000000 -e xai_plugin.prx xai_plugin.sprx
-scetool -0 SELF -1 TRUE -s FALSE -2 04 -3 1070000052000001 -4 01000002 -5 APP -6 0003004000000000 -A 0001000000000000 -e videorec.prx videorec.sprx
-
-pause
